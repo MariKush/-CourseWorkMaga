0fd69f669905257dc55ac1d5dd9b28d971b6f383
MariKush <43176370+MariKush@users.noreply.github.com>
2022-05-23 23:59:05 +0300

    added assertThenUrlContainSearchWord step

--- src/test/java/org/masha/stepDefinitions/SearchByWordSteps.java
+++ src/test/java/org/masha/stepDefinitions/SearchByWordSteps.java
@@ -1,15 +1,24 @@
 package org.masha.stepDefinitions;
 
 
-import io.cucumber.java.en.Given;
+import com.codeborne.selenide.WebDriverRunner;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import org.masha.pageObjects.HeaderElement;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchByWordSteps {
 
     HeaderElement headerElement = new HeaderElement();
 
-    @Given("^I set (.*) value in Search field and press Enter$")
+    @And("^I set (.*) value in Search field and press Enter$")
     public void setValueInSearchFieldAndPressEnter(String searchWord) {
         headerElement.getSearchField().setValue(searchWord).pressEnter();
+    }
+
+    @Then("^I verify that URL contain (.*) word$")
+    public void assertThenUrlContainSearchWord(String searchWord){
+        assertTrue(WebDriverRunner.url().contains(searchWord.toLowerCase()));
     }
 }
