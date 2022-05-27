86de50aa4fe343841e5b99a929e6e735c09ec119
MariKush <43176370+MariKush@users.noreply.github.com>
2022-05-27 23:18:53 +0300

    added Verify cart title and price in the cart scenario

--- src/test/java/org/masha/featureFiles/CartFunctionality.feature
+++ src/test/java/org/masha/featureFiles/CartFunctionality.feature
@@ -5,3 +5,13 @@ Feature: Verify cart functionality
     And I set iPhone value in Search field and press Enter
     And I click add to cart button for the fist good
     Then I verify that cart header counter equals 1
+
+  Scenario: Verify cart title and price in the cart
+    Given I open ROZETKA home page
+    And I set iPhone value in Search field and press Enter
+    And I store first goods title in catalog
+    And I store first goods price in catalog
+    And I click add to cart button for the fist good
+    And I open cart using header
+    Then I verify that goods title in the cart equals to title in the catalog
+    Then I verify that goods price in the cart equals to price in the catalog
--- src/test/java/org/masha/stepDefinitions/CartSteps.java
+++ src/test/java/org/masha/stepDefinitions/CartSteps.java
@@ -3,6 +3,7 @@ package org.masha.stepDefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.masha.pageObjects.CartPage;
 import org.masha.pageObjects.HeaderElement;
 import org.masha.pageObjects.SearchResultsPage;
 
@@ -10,8 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CartSteps {
 
+
+    String firstGoodsTitleInCatalog;
+    Integer firstGoodsPriceInCatalog;
+
     SearchResultsPage searchResultsPage = new SearchResultsPage();
     HeaderElement headerElement = new HeaderElement();
+    CartPage cartPage = new CartPage();
 
     @And("^I click add to cart button for the fist good$")
     public void lickAddToCartButtonForTheFirstGood() {
@@ -20,6 +26,49 @@ public class CartSteps {
 
     @Then("^I verify that cart header counter equals (.*)$")
     public void checkCartHeaderCounter(Integer count) {
-        assertEquals(headerElement.getCartCounter().getText(),count.toString());
+        assertEquals(headerElement.getCartCounter().getText(), count.toString());
     }
+
+    @And("^I store first goods title in catalog$")
+    public void storeFirstGoodsTitleInCatalog() {
+        firstGoodsTitleInCatalog = searchResultsPage.getGoodsTitles().get(0).getText();
+    }
+
+    @And("^I store first goods price in catalog$")
+    public void storeFirstGoodsPriceInCatalog() {
+        firstGoodsPriceInCatalog = searchResultsPage.getGoodsPrices().get(0);
+    }
+
+    @And("^I open cart using header$")
+    public void openCartUsingHeader() {
+        headerElement.getCartButton().click();
+    }
+
+    @Then("^I verify that goods title in the cart equals to title in the catalog$")
+    public void assertGoodsTitle() {
+        assertEquals(cartPage.getGoodTitle().text(), firstGoodsTitleInCatalog);
+    }
+
+    @Then("^I verify that goods price in the cart equals to price in the catalog$")
+    public void assertGoodsPrice() {
+        assertEquals(Integer.parseInt(cartPage.getGoodPrice().getText().replaceAll("[^0-9.]", "")),
+                firstGoodsPriceInCatalog);
+
+    }
+
+
+
+
+
+/*
+
+
+    void checkItemInCart() {
+
+        assertEquals(goodTitle, getCartPage().getGoodTitle());
+        assertEquals(goodPrice, getCartPage().getGoodPrice());
+
+    }
+*/
+
 }
